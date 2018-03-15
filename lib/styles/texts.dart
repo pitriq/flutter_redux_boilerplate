9d670da48e490ceaf29dd33b52c32dfab8370a54
Fran Pitriqueo <franpitri@gmail.com>
2018-03-15 17:43:31 -0300

    update label style

--- lib/styles/texts.dart
+++ lib/styles/texts.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_redux_boilerplate/styles/colors.dart';
-
 Map<String, TextStyle> textStyles = {
-    'bottom_label_selected': new TextStyle(
-        fontSize: 12.0,
-        fontWeight: FontWeight.bold,
-        color: colorStyles['primary_dark'],
-    ),
     'bottom_label': new TextStyle(
         fontSize: 12.0,
-        color: colorStyles['ligth_font'],
+        fontWeight: FontWeight.bold,
     ),
 };
