1dbf3cc2f5339a29d0e378313f9f1f1f039a3a27
Fran Pitriqueo <franpitri@gmail.com>
2018-04-10 18:45:13 -0300

    do some refactor since most dependencies have been updated
    migrated the following packages:
    
    redux: 2.1.1 → 3.0.0 
    flutter_redux: 0.3.5  → 0.5.0
    redux_logging: 0.1.4  → 0.3.0
    redux_thunk: 0.1.1 → 0.2.0
    redux_persist_flutter: 0.4.5 → 0.6.0-rc.1 
    redux_persist: 0.5.1 → 0.7.0-rc.1

--- lib/reducers/app_reducer.dart
+++ lib/reducers/app_reducer.dart
@@ -4,7 +4,8 @@ import 'package:flutter_redux_boilerplate/models/app_state.dart';
 import 'package:flutter_redux_boilerplate/reducers/auth_reducer.dart';
 
 AppState appReducer(AppState state, action){
-    if (action is LoadedAction<AppState>) {
+    //print(action);
+    if (action is PersistLoadedAction<AppState>) {
         return action.state ?? state;
     } else {
         return new AppState(
